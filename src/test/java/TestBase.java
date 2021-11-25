901766495d606235f6aa4592186cd91afdcfc45f
o.chubrick <o.chubrik@corp.mail.ru>
2021-11-25 21:04:16 +0300

    add listener and remote run

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -7,6 +9,8 @@ public class TestBase {
     static void configuration() {
         Configuration.browser = "Chrome";
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
 }
