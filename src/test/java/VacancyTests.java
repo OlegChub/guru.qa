fe5b1da52d69e27576115822ba2c97b69fe6e345
o.chubrick <o.chubrik@corp.mail.ru>
2021-12-22 15:55:11 +0300

    add more tests

--- src/test/java/VacancyTests.java
+++ src/test/java/VacancyTests.java
@@ -1,6 +1,6 @@
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project_config.ProjectConfiguration;
@@ -12,14 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class VacancyTests extends TestBase {
     ProjectConfiguration config = ConfigFactory.create(ProjectConfiguration.class, System.getProperties());
 
-    @BeforeEach
-    void loadVacancyPage() {
-        step("Open vacancies page", () -> open("https://www.souzcvettorg.ru/about/job/"));
-    }
+    String vacanciesPageURL = "https://www.souzcvettorg.ru/about/job/";
+    String contactsPageURL = "https://www.souzcvettorg.ru/about/contacts/";
+    String deliveryPageURL = "https://www.souzcvettorg.ru/about/dostavka-i-oplata/";
+    String shopsPageURL = "https://www.souzcvettorg.ru/about/shop/";
 
     @Test
     @DisplayName("Check page title")
     void checkPageTitle() {
+        step("Open Vacancies page", () -> open(vacanciesPageURL));
         String expectedTitle = "Вакансии Компании СоюзЦветТорг Предложение Работы в Москве | СоюзЦветТорг";
         step("Get actual title and check it", () -> {
             String actualTitle = title();
@@ -30,7 +31,7 @@ public class VacancyTests extends TestBase {
     @Test
     @DisplayName("Check vacancy is present")
     void checkVacancyIsPresent() {
-        //    step("Open vacancies page",()->open("https://www.souzcvettorg.ru/about/job/"));
+        step("Open Vacancies page", () -> open(vacanciesPageURL));
         String vacancyName = "КОНСУЛЬТАНТ ТОРГОВОГО ЗАЛА";
         step("Check vacancy is on the page", () -> {
             $(".nm").shouldHave(Condition.text(vacancyName));
@@ -40,9 +41,38 @@ public class VacancyTests extends TestBase {
     @Test
     @DisplayName("Check header of the page")
     void checkHeaderOfThePage() {
+        step("Open Vacancies page", () -> open(vacanciesPageURL));
         step("Check header of the page", () -> {
             String headerText = "Вакансии Компании СоюзЦветТорг";
             $("#pagetitle").shouldHave(Condition.text(headerText));
+        });
+    }
+
+    @Test
+    @DisplayName("Check phone number on Contacts page")
+    void checkPhoneNumberInContacts() {
+        step("Open Contacts page", () -> open(contactsPageURL));
+        step("Check phone number on the page", () -> {
+            String headerText = "8-800-550-19-00";
+            $(".text_red").shouldHave(Condition.text(headerText));
+        });
+    }
+
+    @Test
+    @DisplayName("Check delivery intervals quantity")
+    void checkDeliveryIntervals() {
+        step("Open Delivery terms page", () -> open(deliveryPageURL));
+        step("Check delivery intervals quantity", () -> {
+            $(".tables-responsive").$$("tr").shouldHave(CollectionCondition.size(13));
+        });
+    }
+
+    @Test
+    @DisplayName("Check shops quantity")
+    void checkShopsQuantity() {
+        step("Open shops page", () -> open(shopsPageURL));
+        step("Check shops quantity", () -> {
+            $(".items").$$(".item").shouldHave(CollectionCondition.size(55));
         });
     }
 }
