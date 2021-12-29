d91988d1145c6d8e544cccbaf91b6edd7e283bab
o.chubrick <o.chubrik@corp.mail.ru>
2021-12-29 16:10:07 +0300

    add CredentialsConfig to api steps

--- src/test/java/api/RestAssuredApiSteps.java
+++ src/test/java/api/RestAssuredApiSteps.java
@@ -1,22 +1,25 @@
 package api;
 
+import config.CredentialsConfig;
 import io.restassured.http.Cookies;
 import io.restassured.response.Response;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 
 public class RestAssuredApiSteps {
-//    CredentialsConfig credentialsConfig= ConfigFactory.create(CredentialsConfig.class,)
-public static Cookies authCookies;
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class, System.getProperties());
+
+    public static Cookies authCookies;
 
     public static Cookies loginAndGetCookies() {
         return authCookies =
                 given()
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .formParam("Email", "alchu@mail.rg")
-                        .formParam("Password", "1q2w3e")
+                        .formParam("Email", config.getEmail())
+                        .formParam("Password", config.getPassword())
                         .when()
                         .post("/login")
                         .then()
--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -2,15 +2,15 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources(
-        "classpath: config/credentials.properties"
-)
+@Config.Sources("classpath: config/credentials.properties")
 public interface CredentialsConfig extends Config {
 
     @Key("email")
+    @DefaultValue("alchu@mail.rg")
     String getEmail();
 
     @Key("password")
+    @DefaultValue("1q2w3e")
     String getPassword();
 
 }
