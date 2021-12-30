c0dd436eb73545dc35b35e27b33894c30ca15d28
o.chubrick <o.chubrik@corp.mail.ru>
2021-12-30 10:58:36 +0300

    add allure steps

--- src/test/java/tests/ApiUiTests.java
+++ src/test/java/tests/ApiUiTests.java
@@ -9,21 +9,42 @@ import static api.RestAssuredApiSteps.authCookies;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class ApiUiTests extends BaseTest {
+    String getWishListItemCounter;
+    Response wishListItemsQuantity;
 
     @Test
     @DisplayName("Add to wishlist and check it on WEB")
     void addToWishList() {
-        RestAssuredApiSteps.loginAndGetCookies();
-        Response wishListItemsQuantity = RestAssuredApiSteps.addToWishListWithResponse(authCookies);
-        String getWishListItemCounter = wishListItemsQuantity.path("updatetopwishlistsectionhtml");
+        step("Login and get cookies", () -> {
+            RestAssuredApiSteps.loginAndGetCookies();
+        });
 
-        open("/Themes/DefaultClean/Content/images/logo.png");
-        RestAssuredApiSteps.addAuthCookieToBrowser();
+        step("Add product to wishlist", () -> {
+            wishListItemsQuantity = RestAssuredApiSteps.addToWishListWithResponse(authCookies);
+        });
 
-        open("");
+        step("Get wishlist goods added quantity", () -> {
+            getWishListItemCounter = wishListItemsQuantity.path("updatetopwishlistsectionhtml");
+        });
 
-        $(".wishlist-qty").shouldHave(text(getWishListItemCounter));
+        step("Open minimal content, because cookie can be set when site is opened", () -> {
+            open("/Themes/DefaultClean/Content/images/logo.png");
+        });
+
+        step("Add auth cookie to browser", () -> {
+            RestAssuredApiSteps.addAuthCookieToBrowser();
+        });
+
+        step("Open main page", () -> {
+            open("");
+        });
+
+        step("Check added items quantity on Web", () -> {
+            $(".wishlist-qty").shouldHave(text(getWishListItemCounter));
+        });
+
     }
 }
