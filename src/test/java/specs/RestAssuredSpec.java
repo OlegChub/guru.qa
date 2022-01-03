69fe8c21c56b501e04f793010d3f5c6735142a83
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-03 11:47:04 +0300

    add comment

--- src/test/java/specs/RestAssuredSpec.java
+++ src/test/java/specs/RestAssuredSpec.java
@@ -7,6 +7,7 @@ import io.restassured.specification.RequestSpecification;
 import static filters.CustomLogFilter.customLogFilter;
 import static io.restassured.RestAssured.with;
 
+//выносим часто используемые настройки запроса в отдельную спеку и далее добавляем метод в given() или через .spec
 public class RestAssuredSpec {
     static public RequestSpecification requestSpecification =
             with()
