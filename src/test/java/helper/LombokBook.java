03e62c9fe1288bd2116bd1cb0e56d1e032241a02
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-04 17:07:02 +0300

    add comments

--- src/test/java/helper/LombokBook.java
+++ src/test/java/helper/LombokBook.java
@@ -5,8 +5,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+    // игнорировать неиспользуемые поля json
 @JsonIgnoreProperties(ignoreUnknown = true)
+/*
+Данный класс для схемы типа
+{
+  "isbn": "9781449325862",
+  "title": "Git Pocket Guide",
+  "subTitle": "A Working Introduction",
+  "author": "Richard E. Silverman"
+}
+ */
 public class LombokBook {
+    // указываем поля, которые будем проверять в json ответе. Lombok сам создает getters/setters
     private String isbn;
     private String title;
     private String author;
