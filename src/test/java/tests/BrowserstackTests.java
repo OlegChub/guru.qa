365005e5802dc2dab54632d9e780883c7a2a59b1
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-18 21:58:02 +0300

    add steps

--- src/test/java/tests/BrowserstackTests.java
+++ src/test/java/tests/BrowserstackTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -8,22 +9,39 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 public class BrowserstackTests extends TestBase {
 
     @Test
+    @DisplayName("Search results list is not empty")
     void checkSearchVariantsArePresent() {
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
-        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        step("Click on search field", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+        });
+        step("Insert search request", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Check that search result not null", () -> {
+            $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
     }
 
 
     @Test
+    @DisplayName("Open page from search results list")
     void checkPageIsPresent() {
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("van Gogh");
-        $(MobileBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/view_page_subtitle_text")).shouldHave(text("Dutch painter (1853–1890)"));
+        step("Click on search field", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+        });
+        step("Insert search request", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("van Gogh");
+        });
+        step("Click on the first link from results list", () -> {
+            $(MobileBy.xpath("//*[@text='Vincent van Gogh']")).click();
+        });
+        step("Check page", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/view_page_subtitle_text")).shouldHave(text("Dutch painter (1853–1890)"));
+        });
     }
 }
