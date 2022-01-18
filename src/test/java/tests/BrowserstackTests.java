0d59d06314383575a10c43b842b6c41bca8acf7d
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-18 19:40:01 +0300

    add 1 test

--- src/test/java/tests/BrowserstackTests.java
+++ src/test/java/tests/BrowserstackTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +9,21 @@ import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class BrowserstackSampleTest extends TestBase {
+public class BrowserstackTests extends TestBase {
 
     @Test
-    void sample() {
+    void checkSearchVariantsArePresent() {
         $(MobileBy.AccessibilityId("Search Wikipedia")).click();
         $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
         $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+    }
+
+
+    @Test
+    void checkPageIsPresent() {
+        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("van Gogh");
+        $(MobileBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/view_page_subtitle_text")).shouldHave(Condition.text("Dutch painter (1853–1890)"));
     }
 }
