030df25100334e53576d8b39bd2dd889e5d05c5c
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-18 16:01:41 +0300

    add Key tags

--- src/test/java/config/ConfigSetup.java
+++ src/test/java/config/ConfigSetup.java
@@ -9,12 +9,15 @@ import org.aeonbits.owner.Config;
 })
 public interface ConfigSetup extends Config {
     @DefaultValue("alchu_5QWANU")
+    @Key("browserstackUser")
     String browserstackUser();
 
     @DefaultValue("yzxWEUPx6sKZNv2GgQJZ")
+    @Key("browserstackKey")
     String browserstackKey();
 
     @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
+    @Key("appLink")
     String appLink();
 
     @DefaultValue("Google Pixel 3")
@@ -24,5 +27,6 @@ public interface ConfigSetup extends Config {
     String osVersion();
 
     @DefaultValue("http://hub.browserstack.com/wd/hub")
+    @Key("browserstackURL")
     String browserstackURL();
 }
