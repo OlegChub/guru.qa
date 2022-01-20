efddcfd96681f821967441931412b6346ab52f14
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-20 17:27:53 +0300

    remove DeviceHostSelector.java

--- src/test/java/tests/emulatorTests/EmulatorTestBase.java
+++ src/test/java/tests/emulatorTests/EmulatorTestBase.java
@@ -2,6 +2,7 @@ package tests.emulatorTests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import driver.EmulatorDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -10,14 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
-import static helpers.DeviceHostSelector.getDeviceHostProperty;
 
 public class EmulatorTestBase {
     @BeforeAll
     public static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.browser = getDeviceHostProperty();
+        Configuration.browser = EmulatorDriver.class.getName();
         Configuration.startMaximized = false;
         Configuration.browserSize = null;
         Configuration.timeout = 10000;
