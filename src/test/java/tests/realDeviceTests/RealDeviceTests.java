0196a21ba3e70d9fe409f207127521e8a42d04e4
o.chubrick <o.chubrik@corp.mail.ru>
2022-01-24 18:08:06 +0300

    add 1 test

--- src/test/java/tests/realDeviceTests/RealDeviceTests.java
+++ src/test/java/tests/realDeviceTests/RealDeviceTests.java
@@ -1,4 +1,29 @@
 package tests.realDeviceTests;
 
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class RealDeviceTests {
+
+    @Test
+    void searchWikipediaTest() {
+        //back();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("New ways to explore"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("Reading lists with sync"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("Send anonymous data"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_container"))
+                .shouldBe(value(String.valueOf(visible)));
+    }
 }
